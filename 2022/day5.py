3cbee203afa2d0f164f04690e0d871fed64a51c4
Ginger Mei <ccaj007@gmail.com>
2022-12-07 19:39:48 -0500

    day 7

--- 2022/day5.py
+++ 2022/day5.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 c1 = ['Z','J','G']
 c2 = ['Q','L','R','P','W','F','V','C']
 c3 = ['F','P','M','C','L','G','R']
@@ -7,7 +9,8 @@ c6 = ['W','H','J','Z','M','Q','T','L']
 c7 = ['H','F','S','B','V']
 c8 = ['F','J','Z','S']
 c9 = ['M','C','D','P','F','H','B','T']
-containers = {1:c1, 2:c2, 3:c3, 4:c4, 5:c5, 6:c6, 7:c7, 8:c8, 9:c9}
+container1 = {1:c1, 2:c2, 3:c3, 4:c4, 5:c5, 6:c6, 7:c7, 8:c8, 9:c9}
+#container2 = deepcopy(container1)
 
 with open("day5-input.txt") as f:
     data = f.read().splitlines()
@@ -15,28 +18,50 @@ with open("day5-input.txt") as f:
 # source = []
 # destin = []
 
-def pop_crate(n,orig, dest):
-    for i in range(n):
-        push_crate(dest, orig.pop())
-
-def push_crate(dest, orig):
-    dest.append(orig)
+# def pop_crate(n,orig, dest):
+#     for i in range(n):
+#         #push_crate(dest, orig.pop())
+#         dest.append(orig.pop())
+#
+# def push_crate(dest, orig):
+#     dest.append(orig)
 
 for i in data:
     cmd = i.split()
     # num.append(cmd[1])
     # destin.append(cmd[5])
     # source.append(cmd[3])
-    num = cmd[1]
-    destin = containers[int(cmd[5])]
-    source = containers[int(cmd[3])]
-    pop_crate(int(num), source, destin)
+    num = int(cmd[1])
+    destin = container1[int(cmd[5])]
+    source = container1[int(cmd[3])]
+    #pop_crate(int(num), source, destin)
+    for _ in range(num):
+        destin.append(source.pop())
 
 print(c1[-1],c2[-1], c3[-1], c4[-1], c5[-1], c6[-1], c7[-1], c8[-1], c9[-1])
 
 #part 2
 
-x = (c2[-2:])
-print(x)
-print(c1.append(c2.pop()))
-print(c1)
+c1 = ['Z','J','G']
+c2 = ['Q','L','R','P','W','F','V','C']
+c3 = ['F','P','M','C','L','G','R']
+c4 = ['L','F','B','W','P','H','M']
+c5 = ['G','C','F','S','V','Q']
+c6 = ['W','H','J','Z','M','Q','T','L']
+c7 = ['H','F','S','B','V']
+c8 = ['F','J','Z','S']
+c9 = ['M','C','D','P','F','H','B','T']
+container2 = {1:c1, 2:c2, 3:c3, 4:c4, 5:c5, 6:c6, 7:c7, 8:c8, 9:c9}
+
+for i in data:
+    cmd = i.split()
+    num = int(cmd[1])
+    destin = container2[(int(cmd[5])-1)]
+    source = container2[(int(cmd[3])-1)][-num:]
+
+    victims = source
+    del source
+
+    destin.extend(victims)
+
+print(c1[-1],c2[-1], c3[-1], c4[-1], c5[-1], c6[-1], c7[-1], c8[-1], c9[-1])
