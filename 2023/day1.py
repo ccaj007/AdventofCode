3750be01907743ddbdf92d890c31ae2cecbe35a1
C L <ccaj007@gmail.com>
2023-12-03 13:16:08 -0500

    day 1

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,18 +1,34 @@
-import re
+#import re
+digit_names = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
-# with open ('day1.txt') as f:
-#     data = f.read().splitlines()
+with open ('day1.txt') as f:
+    data = f.read().splitlines()
 
-# total = 0
+total1 = 0
+total2 = 0
+
+def translate(line):
+    for num, name in enumerate(digit_names):
+        line = line.replace(name, f"{name}{num}{name}")
+    return line
+
 # for i in data:
-#     nums = re.findall(r'\d', i)
+#     nums = re.findall(r'\d', i)    
 #     combine = int(nums[0] + nums[-1])
 #     total += combine
 
-# print(total)
+for line in data:
+    # digits = []
+    # for char in line:
+    #     if char.isnumeric():
+    #         digits.append(char)
+    digits = [char for char in line if char.isnumeric()]
+    total1 += int(digits[0]+digits[-1])
 
-# part 2
-numbers = ('one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine')
+    digits2 = [char for char in translate(line) if char.isnumeric()]
+    total2 += int(digits2[0] + digits[-1])
+    
+print(total1)
+print(total2)
 
-if numbers in 'eightwothree':
-    print(numbers)
+
