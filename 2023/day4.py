267620a1ee54cb2b273e64a45c5b1c44f14e3335
Ginger Mei <ccaj007@gmail.com>
2023-12-11 21:17:35 -0500

    update

--- 2023/day4.py
+++ 2023/day4.py
@@ -1,11 +1,21 @@
-with open("day4-test.txt") as f:
+#with open("day4-test.txt") as f:
+with open("day4.txt") as f:
     lines = f.read().splitlines()
 
-for line in lines:
-    cards, rhs = line.split(":")
-    cards = int(cards.split()[1])
+p1 = p2 = 0
+
+def winner(n):
+    _, rhs = line.split(":")
     win, play = rhs.split("|")
     win = win.split()
     play = play.split()
-    
-    print(cards, win, play)
+    t = len((set(win) & set(play)))
+    return t
+
+for line in lines:
+    t = winner(line)
+    p1 += int(2**(t-1))
+
+print(p1)
+
+# PART 2 TOO CONFUSING; DIDN'T DO
---
+++ 2023/day5.py
@@ -0,0 +1,4 @@
+with open("day5-test.txt") as f:
+    lines = f.read().splitlines()
+
+print(lines)
